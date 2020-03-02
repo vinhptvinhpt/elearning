2e8b7a38d2e5fc43c72c3e4bb2c7b1b1595c4fb4
duythole <leduytho93@gmail.com>
2020-03-02 17:40:35 +0700

    fix layout survey

--- resources/js/components/survey/SurveyEditComponent.vue
+++ resources/js/components/survey/SurveyEditComponent.vue
@@ -1,93 +1,102 @@
 <template>
-  <div class="container-fluid mt-15">
-    <div class="row">
-      <div class="col">
-        <nav class="breadcrumb" aria-label="breadcrumb">
-          <ol class="breadcrumb bg-transparent px-0">
-            <li class="breadcrumb-item"><router-link to="/tms/dashboard">{{ trans.get('keys.dashboard') }}</router-link></li>
-            <li class="breadcrumb-item"><router-link to="/tms/survey/list">{{ trans.get('keys.quan_tri_survey') }}</router-link></li>
-            <li class="breadcrumb-item active">{{ trans.get('keys.chinh_sua_thong_tin_survey') }}</li>
-          </ol>
-        </nav>
-      </div>
-    </div>
-    <div class="row mb-4">
-      <div class="col-sm">
-        <div class="accordion" id="accordion_1">
-          <div class="card">
-            <div class="card-header d-flex justify-content-between">
-              <a role="button" data-toggle="collapse" href="#collapse_1"
-                 aria-expanded="true"><i class="fal fa-plus mr-3"></i>{{trans.get('keys.sua_thong_tin_survey')}}</a>
+    <div class="container-fluid mt-15">
+        <div class="row">
+            <div class="col">
+                <nav class="breadcrumb" aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent px-0">
+                        <li class="breadcrumb-item">
+                            <router-link to="/tms/dashboard">{{ trans.get('keys.dashboard') }}</router-link>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <router-link to="/tms/survey/list">{{ trans.get('keys.quan_tri_survey') }}</router-link>
+                        </li>
+                        <li class="breadcrumb-item active">{{ trans.get('keys.chinh_sua_thong_tin_survey') }}</li>
+                    </ol>
+                </nav>
             </div>
-            <div id="collapse_1" class="collapse show" data-parent="#accordion_1" role="tabpanel">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-12 col-lg-12">
-                    <form action="" class="form-row">
-                      <div class="col-4 form-group">
-                        <label for="inputText1-2">{{trans.get('keys.ma_survey')}} *</label>
-                        <input v-model="survey.code" type="text" id="inputText1-2"
-                               :placeholder="trans.get('keys.nhap_ma')"
-                               class="form-control mb-4">
-                        <label v-if="!survey.code"
-                               class="required text-danger sur_code_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
-                      </div>
-                      <div class="col-4 form-group">
-                        <label for="inputText1-1">{{trans.get('keys.ten_survey')}} *</label>
-                        <input v-model="survey.name" type="text" id="inputText1-1"
-                               :placeholder="trans.get('keys.nhap_ma')"
-                               class="form-control mb-4">
-                        <label v-if="!survey.name"
-                               class="required text-danger sur_name_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
-                      </div>
-
-                      <div class="col-4 form-group">
-                        <label for="inputText6">{{trans.get('keys.thoi_gian_bat_dau')}} *</label>
-                        <input v-model="survey.startdate"" type="date"
-                               id="inputText7"
-                               class="form-control mb-4">
-                        <label v-if="!survey.startdate"
-                               class="required text-danger startdate_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
-                      </div>
-                      <div class="col-4 form-group">
-                        <label for="inputText6">{{trans.get('keys.thoi_gian_ket_thuc')}} *</label>
-                        <input v-model="survey.enddate" type="date"
-                               id="inputText6"
-                               class="form-control mb-4">
-                        <label v-if="!survey.enddate"
-                               class="required text-danger enddate_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
-                      </div>
-
-                      <div class="col-12 form-group">
-                        <label for="inputText6">Mô tả</label>
-                        <textarea v-model="survey.description" class="form-control" rows="10"
-                                  id="article_ckeditor"
-                                  :placeholder="trans.get('keys.noi_dung')"></textarea>
-                      </div>
-                    </form>
-                    <div class="button-list">
-                      <button @click="editSurvey()" type="button" class="btn btn-primary">{{trans.get('keys.sua')}}
-                      </button>
-
-                      <router-link to="/tms/survey/list" class="btn btn-secondary">
-                        {{trans.get('keys.huy')}}
-                      </router-link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-header d-flex justify-content-between">
-              <!--                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse_2"-->
-              <!--                           aria-expanded="false"><i class="fal fa-upload mr-3"></i>Tải lên file Excel</a>-->
-            </div>
-          </div>
         </div>
-      </div>
+        <div class="row mb-4">
+            <div class="col-sm">
+                <div class="accordion" id="accordion_1">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <a role="button" data-toggle="collapse" href="#collapse_1"
+                               aria-expanded="true"><i class="fal fa-plus mr-3"></i>{{trans.get('keys.sua_thong_tin_survey')}}</a>
+                        </div>
+                        <div id="collapse_1" class="collapse show" data-parent="#accordion_1" role="tabpanel">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-12 col-lg-12">
+                                        <form action="" class="form-row">
+                                            <div class="col-4 form-group">
+                                                <label for="inputText1-2">{{trans.get('keys.ma_survey')}} *</label>
+                                                <input v-model="survey.code" type="text" id="inputText1-2"
+                                                       :placeholder="trans.get('keys.nhap_ma')"
+                                                       class="form-control mb-4">
+                                                <label v-if="!survey.code"
+                                                       class="required text-danger sur_code_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
+                                            </div>
+                                            <div class="col-4 form-group">
+                                                <label for="inputText1-1">{{trans.get('keys.ten_survey')}} *</label>
+                                                <input v-model="survey.name" type="text" id="inputText1-1"
+                                                       :placeholder="trans.get('keys.nhap_ma')"
+                                                       class="form-control mb-4">
+                                                <label v-if="!survey.name"
+                                                       class="required text-danger sur_name_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
+                                            </div>
+
+                                            <div class="col-4 form-group">
+                                                <label for="inputText6">{{trans.get('keys.thoi_gian_bat_dau')}}
+                                                    *</label>
+                                                <input v-model="survey.startdate"" type="date"
+                                                id="inputText7"
+                                                class="form-control mb-4">
+                                                <label v-if="!survey.startdate"
+                                                       class="required text-danger startdate_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
+                                            </div>
+                                            <div class="col-4 form-group">
+                                                <label for="inputText6">{{trans.get('keys.thoi_gian_ket_thuc')}}
+                                                    *</label>
+                                                <input v-model="survey.enddate" type="date"
+                                                       id="inputText6"
+                                                       class="form-control mb-4">
+                                                <label v-if="!survey.enddate"
+                                                       class="required text-danger enddate_required hide">{{trans.get('keys.truong_bat_buoc_phai_nhap')}}</label>
+                                            </div>
+
+                                            <div class="col-12 form-group">
+                                                <label for="inputText6">Mô tả</label>
+                                                <!--                        <textarea v-model="survey.description" class="form-control" rows="10"-->
+                                                <!--                                  id="article_ckeditor"-->
+                                                <!--                                  :placeholder="trans.get('keys.noi_dung')"></textarea>-->
+                                                <ckeditor v-model="survey.description"
+                                                          :config="editorConfig"></ckeditor>
+                                            </div>
+                                        </form>
+                                        <div class="button-list">
+                                            <button @click="editSurvey()" type="button" class="btn btn-primary">
+                                                {{trans.get('keys.sua')}}
+                                            </button>
+
+                                            <router-link to="/tms/survey/list" class="btn btn-secondary">
+                                                {{trans.get('keys.huy')}}
+                                            </router-link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <!--                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse_2"-->
+                            <!--                           aria-expanded="false"><i class="fal fa-upload mr-3"></i>Tải lên file Excel</a>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 
 
 </template>
@@ -100,7 +109,16 @@
                 survey: {
                     code: '',
                     name: '',
-                    description: '',
+                    description: ''
+                },
+                editorConfig: {
+                    filebrowserUploadMethod: 'form', //fix for response when uppload file is cause filetools-response-error
+                    // The configuration of the editor.
+                    //add responseType=json for original version of ckeditor 4, else cause filetools-response-error
+                    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+                    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&responseType=json&_token=' + $('meta[name="csrf-token"]').attr('content'),
+                    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+                    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&responseType=json&_token=' + $('meta[name="csrf-token"]').attr('content')
                 }
             }
         },
@@ -158,36 +176,18 @@
                     return;
                 }
 
-                var editor_data = CKEDITOR.instances.article_ckeditor.getData();
                 axios.post('/api/survey/edit/' + +this.survey_id, {
                     sur_code: this.survey.code,
                     sur_name: this.survey.name,
                     startdate: this.survey.startdate,
                     enddate: this.survey.enddate,
-                    description: editor_data
+                    description: this.survey.description
                 }).then(response => {
                     if (response.data.status) {
-                        swal({
-                                title: response.data.message,
-                                // text: response.data.message,
-                                type: "success",
-                                showCancelButton: false,
-                                closeOnConfirm: false,
-                                showLoaderOnConfirm: true
-                            }
-                            , function () {
-                                this.$router.push({ name: 'SurveyIndex' });
-                            }
-                        );
+                        toastr['success'](response.data.message, this.trans.get('keys.thanh_cong'));
+                        this.$router.push({name: 'SurveyIndex'});
                     } else {
-                        swal({
-                            title: response.data.message,
-                            // text: response.data.message,
-                            type: "error",
-                            showCancelButton: false,
-                            closeOnConfirm: false,
-                            showLoaderOnConfirm: true
-                        });
+                        toastr['error'](response.data.message, this.trans.get('keys.that_bai'));
                     }
                 })
                     .catch(error => {
