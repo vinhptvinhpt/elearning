4819b24deb9d5de6401c915eed28c63c86791fdc
datdt <datdt@tinhvan.com>
2020-03-31 17:26:36 +0700

    fix bug ID_14, dashboard

--- resources/js/components/system/organization/IndexOrganizationComponent.vue
+++ resources/js/components/system/organization/IndexOrganizationComponent.vue
@@ -217,7 +217,7 @@
                 current: 1,
                 totalPages: 0,
                 totalRow: 0,
-                row: 10,
+                row: 2,
                 organization_parent_list:[],
                 parent_keyword: '',
                 max_level: 0,
