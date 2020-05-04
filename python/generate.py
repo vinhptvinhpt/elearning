08111b471f7c2bd95de6bbafe1b8a80661bd39d6
duythole <leduytho93@gmail.com>
2020-05-04 11:31:10 +0700

    fix certificate script

--- python/generate.py
+++ python/generate.py
@@ -19,8 +19,8 @@ path = os.getcwd()
 # connect to db
 # path = 'D:\\Job\\elearning-easia\\python'
 # path_gen_img = 'D:\\Job\\elearning-easia\\storage\\app\\public\\upload'
-path = 'E:\\Projects\\elearning-easia\\elearning-easia\\python'
-path_gen_img = 'E:\\Projects\\elearning-easia\\elearning-easia\\storage\\app\\public\\upload'
+path = 'D:\\Work\\TinhVan\\EAsia\\elearning-easia\\python'
+path_gen_img = 'D:\\Work\\TinhVan\\EAsia\\elearning-easia\\storage\\app\\public\\upload'
 bg_size_width = 705
 bg_size_height = 1000
 logo_size_width = 100
@@ -38,20 +38,20 @@ if __name__ == '__main__':
     try:
         if connection.is_connected():
             cursor = connection.cursor()
-            sql_select_image = "select path, position from image_certificate where is_active = 1"
-            # sql_select_image = "select path, position from image_certificate where id = 6"
+            #sql_select_image = "select path, position from image_certificate where is_active = 1"
+            sql_select_image = "select path, position from image_certificate where id = 7"
             cursor = connection.cursor()
             cursor.execute(sql_select_image)
             record_image = cursor.fetchone()
-
+            print(record_image[0])
             # region get image certificate path from db
             link_image = record_image[0].replace('/storage/upload/', '')
             path_image = os.path.join(path_gen_img, link_image)
-
+            print('50')
             # open image
             img = Image.open(path_image)
             img = img.resize((bg_size_width, bg_size_height), Image.ANTIALIAS)
-
+            print('54')
             # width and height of image
             image_width, image_height = img.size
             #
@@ -86,7 +86,7 @@ if __name__ == '__main__':
             image_new_width = coordinates["image_width"]
             image_new_height = coordinates["image_height"]
 
-
+            print('89')
             # region get new position
             positon_name_X = image_width * fullnameX / image_new_width - 20
             positon_name_Y = image_height * fullnameY / image_new_height
@@ -110,76 +110,91 @@ if __name__ == '__main__':
             font_training = ImageFont.truetype(os.path.join(path, 'Lato-Bold.ttf'), size=font_size_program_new)
             font_date = ImageFont.truetype(os.path.join(path, 'Lato-Bold.ttf'), size=font_size_date_new)
             # endregion
+            print('113')
 
             if (get_user_id == 0):
                 if (get_training_id == 0):
-                    sql_select_Query = "select tms_user_detail.user_id, tms_user_detail.fullname as fullname, tms_traninning_programs.name as name, tms_traninning_programs.logo as logo, student_certificate.timecertificate as timecertificate, student_certificate.code as code, student_certificate.id as student_certificate_id from student_certificate join tms_user_detail on student_certificate.userid = tms_user_detail.user_id join tms_traninning_programs on tms_traninning_programs.id = student_certificate.trainning_id where student_certificate.status = 1 "
+                    #sql_select_Query = "select tms_user_detail.user_id, tms_user_detail.fullname as fullname, tms_traninning_programs.name as name, tms_traninning_programs.logo as logo, student_certificate.timecertificate as timecertificate, student_certificate.code as code, student_certificate.id as student_certificate_id from student_certificate join tms_user_detail on student_certificate.userid = tms_user_detail.user_id join tms_traninning_programs on tms_traninning_programs.id = student_certificate.trainning_id where student_certificate.status = 1 "
+                    sql_select_Query = "select tud.user_id, tud.fullname as fullname, ttp.name as name,ttp.logo as logo, sc.timecertificate as timecertificate, sc.code as code, sc.id as sc_id, toe.organization_id,ttp.auto_certificate as auto_certificate,ttp.auto_badge as auto_badge from student_certificate as sc join tms_user_detail as tud on sc.userid = tud.user_id join tms_organization_employee as toe on toe.user_id = tud.user_id join tms_traninning_programs as ttp on ttp.id = sc.trainning_id where sc.status = 1"
                 else:
                     sql_select_Query = "select tms_user_detail.user_id, tms_user_detail.fullname as fullname, tms_traninning_programs.name as name, tms_traninning_programs.logo as logo, student_certificate.timecertificate as timecertificate, student_certificate.code as code, student_certificate.id as student_certificate_id from student_certificate join tms_user_detail on student_certificate.userid = tms_user_detail.user_id join tms_traninning_programs on tms_traninning_programs.id = student_certificate.trainning_id where student_certificate.status = 1 and student_certificate.trainning_id = " + str(
                         get_training_id)
                 cursor.execute(sql_select_Query)
                 records = cursor.fetchall()
+                print('124')
                 for row in records:
+                    print(row)
                     # 19/3/2020 uydd
                     user_id = row[0]
                     name = row[1].encode('utf-8').strip()
                     training_name = row[2]
+                    print(row[3])
                     link_logo = row[3].replace('/storage/upload/', '')
+                    print('129')
                     timecertificate = row[4]
                     code = row[5]
                     student_certificate_id = row[6]
                     path_logo = os.path.join(path_gen_img, link_logo)
+                    print(path_logo)
+
+                    auto_cer = row[8]
+                    auto_badge = row[9]
                     try:
-                        logo = Image.open(path_logo)
-                        logo = logo.resize((logo_size_width, logo_size_height), Image.ANTIALIAS)
-                        # name to utf8
-                        name_utf8 = name.decode('utf8')
-                        name_utf8_training = training_name
+                        if auto_cer == 1:
+                            #logo = Image.open(path_logo)
+                            print('137')
+                            #logo = logo.resize((logo_size_width, logo_size_height), Image.ANTIALIAS)
+                            # name to utf8
+                            name_utf8 = name.decode('utf8')
+                            name_utf8_training = training_name
+                            print('138')
+                            # open image
+                            img = Image.open(path_image)
+                            img = img.resize((bg_size_width, bg_size_height), Image.ANTIALIAS)
+                            print('142')
+                            # region fill text
+                            # create the canvas
+                            canvas = ImageDraw.Draw(img)
 
-                        # open image
-                        img = Image.open(path_image)
-                        img = img.resize((bg_size_width, bg_size_height), Image.ANTIALIAS)
+                            # set datetime
+                            now = datetime.datetime.now()
+                            strDate = str(now.day) + '/' + str(now.month) + '/' + str(now.year)
+                            canvas.text((positon_date_X, positon_date_Y), strDate, font=font_date, fill='#111111')
+                            # endregion
 
-                        # region fill text
-                        # create the canvas
-                        canvas = ImageDraw.Draw(img)
+                            # # get width and height of text
+                            # name_width, name_height = canvas.textsize(name_utf8, font=font_name)
+                            # training_width, training_height = canvas.textsize(name_utf8_training, font=font_training)
 
-                        # set datetime
-                        now = datetime.datetime.now()
-                        strDate = str(now.day) + '/' + str(now.month) + '/' + str(now.year)
-                        canvas.text((positon_date_X, positon_date_Y), strDate, font=font_date, fill='#111111')
-                        # endregion
+                            # set name to position
+                            canvas.text((positon_program_X, positon_program_Y), name_utf8_training, font=font_training, fill='#111111')
+                            # canvas.text((2105, 1585), address_utf8_branch, font=font_name, fill='#060836')
+                            # canvas.text((positon_name_X, positon_name_Y),name_utf8.upper(), font=font_name, fill='#111111')
+                            width_cut = int(fullnameWidth / fullnameSize) * 2 + 2
+                            height_text = positon_name_Y
+                            if fullnameHeight > fullnameSize:
+                                for line in textwrap.wrap(name_utf8.encode('utf8'), width=width_cut):
+                                    canvas.text((positon_name_X, height_text), line.decode('utf8'), font=font_name,
+                                                fill='#111111')
+                                    height_text += font_name.getsize(line)[1] + 15
+                            else:
+                                canvas.text((positon_name_X, positon_name_Y), name_utf8, font=font_name, fill='#111111')
 
-                        # # get width and height of text
-                        # name_width, name_height = canvas.textsize(name_utf8, font=font_name)
-                        # training_width, training_height = canvas.textsize(name_utf8_training, font=font_training)
+                            #img.paste(logo, (positon_logo_X, positon_logo_Y))
 
-                        # set name to position
-                        canvas.text((positon_program_X, positon_program_Y), name_utf8_training, font=font_training, fill='#111111')
-                        # canvas.text((2105, 1585), address_utf8_branch, font=font_name, fill='#060836')
-                        # canvas.text((positon_name_X, positon_name_Y),name_utf8.upper(), font=font_name, fill='#111111')
-                        width_cut = int(fullnameWidth / fullnameSize) * 2 + 2
-                        height_text = positon_name_Y
-                        if fullnameHeight > fullnameSize:
-                            for line in textwrap.wrap(name_utf8.encode('utf8'), width=width_cut):
-                                canvas.text((positon_name_X, height_text), line.decode('utf8'), font=font_name,
-                                            fill='#111111')
-                                height_text += font_name.getsize(line)[1] + 15
-                        else:
-                            canvas.text((positon_name_X, positon_name_Y), name_utf8, font=font_name, fill='#111111')
+                            # save image
+                            img.save(os.path.join(path_gen_img, 'certificate', code + '.png'))
 
-                        img.paste(logo, (positon_logo_X, positon_logo_Y))
-
-                        # save image
-                        img.save(os.path.join(path_gen_img, 'certificate', code + '.png'))
+                        if auto_badge == 1:
+                            print('auto gen badge')
                         sql = """UPDATE student_certificate SET status = 2 WHERE id = %s"""
-                        cursor.execute(sql, (student_certificate_id,))
+                        #cursor.execute(sql, (student_certificate_id,))
                         sql_update_confirm = """UPDATE tms_user_detail SET confirm = 1 WHERE tms_user_detail.id = %s"""
-                        cursor.execute(sql_update_confirm, (get_user_id,))
+                        #cursor.execute(sql_update_confirm, (get_user_id,))
                     except Exception, e:  # xu ly chuyen trang thai cho cac ban ghi bi loi
-                        sql = """UPDATE student_certificate SET status = 3 WHERE id = %s"""
-                        cursor.execute(sql, (student_certificate_id,))
                         print(e)
+                        sql = """UPDATE student_certificate SET status = 3 WHERE id = %s"""
+                        #cursor.execute(sql, (student_certificate_id,))
                 connection.commit()
                 print('luu nhieu anh thanh cong')
             else:
