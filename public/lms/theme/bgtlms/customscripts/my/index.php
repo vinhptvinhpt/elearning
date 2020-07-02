1e021ea0860e7cfdbd60ff45a8001b8dcdf1f489
datdt <datdt@tinhvan.com>
2020-07-02 14:56:37 +0700

    update favicon

--- public/lms/theme/bgtlms/customscripts/my/index.php
+++ public/lms/theme/bgtlms/customscripts/my/index.php
@@ -199,6 +199,7 @@ $percentStudying = intval(count($courses_current) * 100 / count($courses));
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <base href="../../">
+<link rel="shortcut icon" href="images/favicon.png">
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/font-awesome.min.css">
 <script src="js/jquery.min.js"></script>
