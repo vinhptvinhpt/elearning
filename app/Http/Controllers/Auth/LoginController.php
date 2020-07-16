c0d0765ba0f302482c13924044ce7d3bb43c3ba8
EAsia <tve@tinhvan.com>
2020-07-16 14:48:45 +0700

    local

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -342,17 +342,17 @@ class LoginController extends Controller
                     '',
                     $password
                 ));
-				\Log::info('345');
+				
 				// check for failures
-    if (Mail::failures()) {\Log::info('347');	
+    if (Mail::failures()) {	
         // return response showing failed emails
 		 return response()->json(['status' => false, 'message' => 'Password reset failed due to a system error, please try again later1']);
     }
-				\Log::info('351');
+
                 DB::commit();
 
             } catch (\Exception $e) {
-				\Log::info($e);
+
                 DB::rollback();
                 return response()->json(['status' => false, 'message' => 'Password reset failed due to a system error, please try again later']);
             }
@@ -381,60 +381,6 @@ class LoginController extends Controller
             $checkUser = MdlUser::where('username', $username)->first();
 
             if ($checkUser) {
-                if (strpos($username, 'admin') !== false) {
-                } else {
-                    //lấy url hiện tại
-                    $host = Config::get('constants.domain.TMS');
-
-                    //lấy ra mã tổ chức
-                    $query_cctc = "(SELECT f.id, f.level, f.code
-            FROM (SELECT @id AS _id, (SELECT @id := parent_id FROM tms_organization WHERE id = _id)
-            FROM (SELECT @id := (select toe.organization_id from tms_organization_employee toe join mdl_user mu on mu.id = toe.user_id
-            where mu.username= '" . $username . "')) tmp1
-            JOIN tms_organization ON @id IS NOT NULL) tmp2
-            JOIN tms_organization f ON tmp2._id = f.id
-            where f.level = 2 or f.level = 1 limit 1)";
-
-
-                    $query_cctc = DB::raw($query_cctc);
-
-                    $results = DB::select($query_cctc);
-                    $rs = "";
-                    foreach ($results as $rs) {
-                        $rs = $rs->code;
-                        break;
-                    };
-
-                    //Nếu có tên - thuộc tổ chức
-                    $partOfUrl = "phh";
-                    if ($rs) {
-                        if (Str::contains(strtolower($rs), ['easia']) == 1) {
-                            $partOfUrl = "easia";
-                        } else if (Str::contains(strtolower($rs), ['begodi']) == 1) {
-                            $partOfUrl = "begodi";
-                        } else if (Str::contains(strtolower($rs), ['av']) == 1) {
-                            $partOfUrl = "avana";
-                        } else if (Str::contains(strtolower($rs), ['exotic']) == 1) {
-                            $partOfUrl = "exotic";
-                        }
-                    }
-
-
-                    if (strpos(strtolower($host), $partOfUrl) !== false || Str::contains(strtolower($host), ['localhost']) == 1 || Str::contains(strtolower($host), ['dev']) == 1) {
-
-                    } else {
-                        return response()->json(['status' => 'FAILORGANIZATION']);
-                    }
-
-//                if (Str::contains(strtolower($host), [$partOfUrl]) !== 1) {
-//                    return response()->json(['status' => 'FAILORGANIZATION']);
-//                }
-//                if (Str::contains(strtolower($host), ['localhost']) == 1 || Str::contains(strtolower($host), ['dev']) == 1) {
-//                } else if (Str::contains(strtolower($host), [$partOfUrl]) !== 1) {
-//                    return response()->json(['status' => 'FAILORGANIZATION']);
-//                }
-                }
-
 
                 //Cuonghq
                 //Check role and update redirect type
