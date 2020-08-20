db4c66d0205a4f3dcef7df52a2409a404638bc4b
datdt <datdt@tinhvan.com>
2020-08-20 15:35:14 +0700

    [style] update style lms

--- public/lms/theme/bgtlms/customscripts/my/index.php
+++ public/lms/theme/bgtlms/customscripts/my/index.php
@@ -940,6 +940,14 @@ $_SESSION["allowCms"] = $allowCms;
         }
     }
 
+    @media screen and (max-width: 1920px) {
+        .info-user .avatar img {
+            width: 105px !important;
+            height: 105px !important;
+            bottom: 26% !important;
+        }
+    }
+
     @media screen and (max-width: 1024px) {
         .block-color {
             width: 80px !important;
