a6ace531334e8a3327ded22ede536306f8c0f941
datdt <datdt@tinhvan.com>
2020-09-15 10:09:47 +0700

    [fix] check if not exists auth.user then logout

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -5,7 +5,7 @@
 
     <meta property="og:title" content="Elearning" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="/images/favicon.png" />
+    <meta property="og:image" content="/images/favicon.ico" />
     <meta property="og:description" content="Elearning" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
