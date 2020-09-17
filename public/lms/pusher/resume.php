a3ce9a420414ce0669edbdeeb84301f9db3dd076
Cuong Hy <cuong@tinhvan.com>
2020-09-17 14:07:01 +0700

    [LMS][Fix] Update resume.php

--- public/lms/pusher/resume.php
+++ public/lms/pusher/resume.php
@@ -11,10 +11,10 @@ if (!isguestuser()) {     // Guests can never edit their profile.
         $sql = "SELECT * FROM tms_learning_activity_logs
 INNER JOIN mdl_enrol me on tms_learning_activity_logs.course_id = me.courseid AND me.roleid = 5
 INNER JOIN mdl_user_enrolments mue on me.id = mue.enrolid
-WHERE user_id = $userid
-AND course_id = $course_id
-AND studying = 1
-AND mue.userid = 23882
+WHERE tms_learning_activity_logs.user_id = $userid
+AND tms_learning_activity_logs.course_id = $course_id
+AND tms_learning_activity_logs.studying = 1
+AND mue.userid = $userid
 ORDER BY id DESC
 LIMIT 1";
         $log = $DB->get_records_sql($sql);
