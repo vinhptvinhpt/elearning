758522c4fee77e544849e423dbaf6315c33762bf
Cuong Hy <cuong@tinhvan.com>
2020-09-21 11:33:26 +0700

    [LMS] Update check enrolled in course detail when learn option course

--- public/lms/pusher/resume.php
+++ public/lms/pusher/resume.php
@@ -9,7 +9,7 @@ if (!isguestuser()) {     // Guests can never edit their profile.
 
     if ($userid != 0 && $course_id != 0) { //Check enrol nếu chưa enrol thì không trả về thông báo nữa
         $sql = "SELECT * FROM tms_learning_activity_logs
-INNER JOIN mdl_enrol me on tms_learning_activity_logs.course_id = me.courseid AND me.roleid = 5
+INNER JOIN mdl_enrol me on tms_learning_activity_logs.course_id = me.courseid AND me.roleid = 5 AND me.enrol <> 'self'
 INNER JOIN mdl_user_enrolments mue on me.id = mue.enrolid
 WHERE tms_learning_activity_logs.user_id = $userid
 AND tms_learning_activity_logs.course_id = $course_id
