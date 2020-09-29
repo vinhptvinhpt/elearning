e8aaa960e75133dbcc21422de780d46ed19d8f5e
vinhpt@tinhvan.com <vinhpt@tinhvan.com>
2020-09-29 14:59:49 +0700

    [LMS][DEMO] Switch case display AIC

--- public/lms/theme/bgtlms/customscripts/my/index.php
+++ public/lms/theme/bgtlms/customscripts/my/index.php
@@ -49,9 +49,11 @@ if (strpos($organizationLower, 'bg') === 0 || strpos($organizationLower, 'begodi
     $organizationCodeGet = "AV";
 } else if (strpos($organizationLower, 'tve') === 0) {
     $organizationCodeGet = "TVE";
+} else if (strpos($organizationLower, 'aic') === 0) {
+    $organizationCodeGet = "AIC";
 //    $organization_id = 0;
 } else {
-    $organizationCodeGet = "PHH";
+    $organizationCodeGet = "AIC";
 }
 
 //color for h1 - organization
@@ -117,6 +119,19 @@ switch ($organizationCode) {
             $_SESSION["component"] = 'images/cpn-vnu.png';
             $_SESSION["pathImgAvailableCourse"] = 'images/2a.png';
             $_SESSION["pathBackground"] = 'images/1a.png';
+            $className = 'Elearning';
+            $colorOrganizationText = '#000';
+        }
+        break;
+    case "AIC":
+        {
+            $_SESSION["organizationName"] = 'AIC';
+            $_SESSION["color"] = '#cf3e3e';
+            $_SESSION["pathLogo"] = 'images/phh.png';
+            $_SESSION["pathLogoWhite"] = 'images/Logo-AIC.png';
+            $_SESSION["component"] = 'images/cpn-aic.png';
+            $_SESSION["pathImgAvailableCourse"] = 'images/2c.png';
+            $_SESSION["pathBackground"] = 'images/2b.jpg';
             $className = 'Elearning';
             $colorOrganizationText = '#000';
         }
