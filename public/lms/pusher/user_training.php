139cce0ffaa00902b706d9637c23f45e0802ba11
Cuong Hy <cuong@tinhvan.com>
2020-10-07 16:13:58 +0700

    [LMS][Fix] Training list in profile

--- public/lms/pusher/user_training.php
+++ public/lms/pusher/user_training.php
@@ -8,8 +8,7 @@ ttp.id,
 ttp.name
 from tms_traninning_users ttu
 inner join tms_traninning_programs ttp on ttp.id = ttu.trainning_id
-where me.enrol = "manual"
-and ttp.deleted = 0
+where ttp.deleted = 0
 and ttu.user_id = ' . $USER->id;
 
 $list = array_values($DB->get_records_sql($sql)); //Auto group by moodle
