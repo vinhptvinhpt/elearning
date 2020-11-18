999bf370e3f933f84a81e171d5f89255875229d6
duythole <leduytho93@gmail.com>
2020-11-18 16:51:19 +0700

    [FIX] update intergate histaff

--- app/Http/Controllers/Backend/HiStaff/SyncDataController.php
+++ app/Http/Controllers/Backend/HiStaff/SyncDataController.php
@@ -34,151 +34,107 @@ class SyncDataController
         try {
             $username = $request->input('username');
             $token_histaff = $request->input('token_histaff');
-            $password = $request->input('password');
+
+            $param = [
+                'username' => 'text'
+            ];
+            $validator = validate_fails($request, $param);
+            if (!empty($validator)) {
+                return response()->json(['status' => 'FAIL']);
+            }
+
+            if (!$username)
+                return response()->json(['status' => 'FAIL']);
+
 
             //xac thuc token voi histaff
             $url = Config::get('constants.domain.HISTAFF-API') . 'CheckToken';
 
             $result_api = callAPIHiStaff('POST', $url, $username, $token_histaff, Config::get('constants.domain.HISTAFF-KEY'));
+
             $result_api = json_decode($result_api, true);
 
             if ($result_api['Code'] != '200') {
                 return response()->json(['status' => 'FAILTOKEN']);
             }
 
+            $checkUser = MdlUser::where('username', $username)->first();
 
-            $checkUser = MdlUser::where(DB::raw('LOWER(`username`)'), strtolower($username))->first();
+            if ($checkUser) {
+                //Cuonghq
+                //Check role and update redirect type
+                $sru = DB::table('model_has_roles as mhr')
+                    ->join('roles', 'roles.id', '=', 'mhr.role_id')
+                    ->leftJoin('permission_slug_role as psr', 'psr.role_id', '=', 'mhr.role_id')
+                    ->join('mdl_user as mu', 'mu.id', '=', 'mhr.model_id')
+                    ->where('mhr.model_id', $checkUser->id)
+                    ->where('mhr.model_type', 'App/MdlUser')
+                    ->get();
 
+                $current_redirect_type = $checkUser->redirect_type;
 
-            if (!$checkUser) {
-                return response()->json(['status' => 'FAILUSER']);
-            }
-
-            if ($checkUser->active == 0) {
-                return response()->json(['status' => 'FAILBANNED']);
-            }
-
-            if (strpos($username, 'admin') !== false) {
-            } else {
-                //lấy url hiện tại
-                $host = Config::get('constants.domain.TMS');
-
-                //lấy ra mã tổ chức
-                $query_cctc = "(SELECT f.id, f.level, f.code
-            FROM (SELECT @id AS _id, (SELECT @id := parent_id FROM tms_organization WHERE id = _id)
-            FROM (SELECT @id := (select toe.organization_id from tms_organization_employee toe join mdl_user mu on mu.id = toe.user_id
-            where mu.username= '" . $username . "')) tmp1
-            JOIN tms_organization ON @id IS NOT NULL) tmp2
-            JOIN tms_organization f ON tmp2._id = f.id
-            where f.level = 2 or f.level = 1 limit 1)";
-
-
-                $query_cctc = DB::raw($query_cctc);
-
-                $results = DB::select($query_cctc);
-                $rs = "";
-                foreach ($results as $rs) {
-                    $rs = $rs->code;
-                    break;
-                };
-
-                //Nếu có tên - thuộc tổ chức
-                $partOfUrl = "phh";
-                if ($rs) {
-                    if (Str::contains(strtolower($rs), ['ea']) == 1) {
-                        $partOfUrl = "easia";
-                    } else if (Str::contains(strtolower($rs), ['bg']) == 1) {
-                        $partOfUrl = "begodi";
-                    } else if (Str::contains(strtolower($rs), ['av']) == 1) {
-                        $partOfUrl = "avana";
-                    } else if (Str::contains(strtolower($rs), ['ev']) == 1) {
-                        $partOfUrl = "exotic";
+                $redirect_type = 'lms';
+                if (count($sru) != 0) {
+                    foreach ($sru as $role) {
+                        if (!in_array($role->name, [Role::STUDENT, Role::ROLE_EMPLOYEE])) {
+                            $redirect_type = "default";
+                            break;
+                        }
                     }
                 }
 
-
-                if (strpos(strtolower($host), $partOfUrl) !== false || Str::contains(strtolower($host), ['localhost']) == 1 || Str::contains(strtolower($host), ['dev']) == 1) {
-
-                } else {
-                    return response()->json(['status' => 'FAILORGANIZATION']);
+                $updated = false;
+                //Update redirect type
+                if ($redirect_type != $current_redirect_type) {
+                    $checkUser->redirect_type = $redirect_type;
+                    $updated = true;
                 }
-            }
 
+                // [VinhPT]
+                // Get description for user check
+                $response['redirect_type'] = $redirect_type;
 
-            // grab credentials from the request
-            $credentials = $request->only('username', 'password');
-            if (!$token = JWTAuth::attempt($credentials)) {
-                return response()->json(['status' => 'invalid_credentials'], 401);
-            }
-
-            //Check role and update redirect type
-            $sru = DB::table('model_has_roles as mhr')
-                ->join('roles', 'roles.id', '=', 'mhr.role_id')
-                ->leftJoin('permission_slug_role as psr', 'psr.role_id', '=', 'mhr.role_id')
-                ->join('mdl_user as mu', 'mu.id', '=', 'mhr.model_id')
-                ->where('mhr.model_id', $checkUser->id)
-                ->where('mhr.model_type', 'App/MdlUser')
-                ->get();
-
-            $current_redirect_type = $checkUser->redirect_type;
-
-            $redirect_type = 'lms';
-            if (count($sru) != 0) {
-                foreach ($sru as $role) {
-                    if (!in_array($role->name, [Role::STUDENT, Role::ROLE_EMPLOYEE])) {
-                        $redirect_type = "default";
-                        break;
-                    }
+                if (empty($checkUser->token)) {
+                    $token = compact('token');
+                    $checkUser->token = $token['token'];
+                    $updated = true;
                 }
+
+                if ($updated) { //Luu thong tin moi
+                    $checkUser->save();
+                }
+
+                $token = $checkUser->token;
+
+
+                Auth::login($checkUser);
+
+                //set token cho phep nhan dang dau la user cua histaff
+                setcookie(Config::get('constants.domain.HISTAFF-COOKIE'), $token_histaff, time() + 3600, '/', Config::get('constants.domain.DOMAIN-COOKIE'), false, false);
+
+                $response['jwt'] = $token;
+                $response['status'] = 'SUCCESS';
+                // [VinhPT]
+                // Get description for user check
+                $response['redirect_type'] = $checkUser->redirect_type;
+                //encrypt for login lms
+                $app_name = Config::get('constants.domain.APP_NAME');
+
+                $key_app = encrypt_key($app_name);
+
+                $data_lms = array(
+                    'user_id' => $checkUser->id,
+                    'app_key' => $key_app
+                );
+
+                $data_lms = createJWT($data_lms, 'data');
+
+                $response['data'] = $data_lms;
+                return response()->json($response);
+
             }
 
-            $updated = false;
-            //Update redirect type
-            if ($redirect_type != $current_redirect_type) {
-                $checkUser->redirect_type = $redirect_type;
-                $updated = true;
-            }
-
-            // Get description for user check
-            $response['redirect_type'] = $redirect_type;
-
-            if (empty($checkUser->token)) {
-                $token = compact('token');
-                $checkUser->token = $token['token'];
-                $updated = true;
-            }
-
-            if ($updated) { //Luu thong tin moi
-                $checkUser->save();
-            }
-
-            $token = $checkUser->token;
-
-            Auth::login($checkUser, 1);
-
-            $response['id'] = $checkUser->id;
-            $response['username'] = $checkUser->username;
-            $response['avatar'] = Auth::user()->detail['avatar'];
-            $response['fullname'] = Auth::user()->detail['fullname'];
-            $response['domain'] = Config::get('constants.domain.TMS');
-            $response['jwt'] = $token;
-            $response['status'] = 'SUCCESS';
-
-            //encrypt for login lms
-            $app_name = Config::get('constants.domain.APP_NAME');
-
-            $key_app = encrypt_key($app_name);
-
-            $data_lms = array(
-                'user_id' => $checkUser->id,
-                'app_key' => $key_app
-            );
-
-            $data_lms = createJWT($data_lms, 'data');
-
-            $response['data'] = $data_lms;
-
-            return response()->json($response);
+            return response()->json(['status' => 'FAIL']);
 
         } catch (Exception $e) {
             return response()->json(['status' => 'FAIL']);
